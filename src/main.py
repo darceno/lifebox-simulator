1ea15403968da9e7e30d5af81d16e410f2fdbab1
darceno <123845571+darceno@users.noreply.github.com>
2023-04-29 21:40:50 -0300

    Create Organism parent class

--- src/main.py
+++ src/main.py
@@ -11,6 +11,18 @@ screen = pygame.display.set_mode((WIDTH_SIZE, HEIGHT_SIZE), pygame.SCALED | pyga
 pygame.display.set_caption("Cell Simulation")
 clock = pygame.time.Clock()
 
+# Organisms parent class
+class ORGANISM:
+
+    def __init__(self, x, y, color, size=15):
+        self.x = random.randint(20, WIDTH_SIZE-20)
+        self.y = random.randint(20, HEIGHT_SIZE-20)
+        self.size = size
+        self.color = color
+
+    def draw(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size) 
+
 # Simulation main loop
 def main():
     run = True
