42fc561e2894a6dcc937f84b107f97dec6183f23
darceno <123845571+darceno@users.noreply.github.com>
2023-05-06 01:25:21 -0300

    Change chance for successful reproduction to 80%

--- src/main.py
+++ src/main.py
@@ -85,7 +85,7 @@ class Organism:
     
     def asexual_reproduction(self):
         if self.energy >= len(self.genome) + 1:
-            if random.randint(1, 2) == 2:
+            if random.randint(1, 10) >= 8:
                 offspring = Organism(self.x, self.y, "blue", self.genome)
                 simulation.spawn_offsprings(offspring)
                 self.energy -= len(self.genome)
