0ef8fb3e2dca40b1581fdf96f72a96325fde0b99
darceno <123845571+darceno@users.noreply.github.com>
2023-05-06 01:26:39 -0300

    Change default 'year' value to 30 seconds

--- src/settings.py
+++ src/settings.py
@@ -8,7 +8,7 @@ WIDTH_SIZE, HEIGHT_SIZE = 1280, 720 # default = 1280, 720
 FPS = 30 # default = 30
 BG_COLOR = (21, 36, 36) # default = (21, 36, 36)
 STARTING_POPULATION = 10 # default = 10
-YEAR = 15 # default = 15
+YEAR = 30 # default = 30
 
 # ORGANISMS SETTINGS
 SIZE_BY_GENE = 5 # default = 5
