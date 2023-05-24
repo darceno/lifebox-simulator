88d10c9c1eecccfd1913b98c3f739627b9271124
darceno <123845571+darceno@users.noreply.github.com>
2023-05-24 12:07:44 -0300

    Window automatically center on the screen

--- src/main_arcade.py
+++ src/main_arcade.py
@@ -4,7 +4,7 @@ from settings import *
 
 class Main(arcade.Window):
     def __init__(self):
-        super().__init__(WIDTH_SIZE, HEIGHT_SIZE, title="LifeBox Simulator", resizable=True)
+        super().__init__(WIDTH_SIZE, HEIGHT_SIZE, title="LifeBox Simulator", resizable=True, center_window=True)
 
         arcade.set_background_color((21, 36, 36))
 
