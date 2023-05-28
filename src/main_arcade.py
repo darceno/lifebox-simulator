5cee1c25a5d795c301f030e11f04338e37a0adc5
darceno <123845571+darceno@users.noreply.github.com>
2023-05-27 23:18:50 -0300

    Add create_organisms function

--- src/main_arcade.py
+++ src/main_arcade.py
@@ -1,4 +1,5 @@
 import arcade
+import random
 
 from settings import *
 
@@ -14,17 +15,25 @@ class Simulation(arcade.Window):
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title, center_window=True)
+        self.organisms = None
 
         arcade.set_background_color((21, 36, 36))
 
     def setup(self):
-        pass
+        self.organisms = arcade.SpriteList()
+        self.create_organisms()
 
     def on_draw(self):
         self.clear()
+        self.organisms.draw()
 
     def on_update(self, delta_time):
         pass
+
+    def create_organisms(self):
+        for i in range(STARTING_POPULATION):
+            organism = Organism(random.randrange(WIDTH_SIZE-10), random.randrange(HEIGHT_SIZE-10), 7, "white", ["CR", "RA"])
+            self.organisms.append(organism)
 
 def main():
     simulation = Simulation(WIDTH_SIZE, HEIGHT_SIZE, "LifeBox Simulator")
