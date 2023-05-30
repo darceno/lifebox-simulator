7427633c43840699ddb7780b5e521b764c9fdfaa
darceno <123845571+darceno@users.noreply.github.com>
2023-05-30 10:24:57 -0300

    Move pygame files to 'old' folder

---
+++ src/old/settings.py
@@ -0,0 +1,32 @@
+"""
+Here you can edit most of the variables that are used as parameters in the simulation. 
+
+"""
+
+# SIMULATION SETTINGS
+WIDTH_SIZE, HEIGHT_SIZE = 1280, 720 # default = 1280, 720
+FPS = 60 # default = 60
+BG_COLOR = (21, 36, 36) # default = (21, 36, 36)
+STARTING_POPULATION = 10 # default = 10
+YEAR = 30 # default = 30
+
+# ORGANISMS SETTINGS
+STARTING_SIZE = 7 # default = 7
+SIZE_BY_GENE = 1 # default = 1
+STARTING_COLOR = "white" # default = "white"
+SPEED = 42 # default = 42
+MUTATION_CHANCE = 30 # default = 30
+
+# INFO DISPLAY SETTINGS
+enable_info_display = True # default = True
+font_size = 30 # default = 30
+font_color = "white" # default = "white"
+anti_aliasing = True # default = True
+show_FPS = True # default = True
+show_population = True # default = True
+show_time = True # defatult = True
+
+# DEBUG INFO SETTINGS
+debug_font_size = 30 # default = 30
+debug_font_color = "yellow" # default = "yellow"
+debug_background_color = "black" # default = "black"
